15b8fbac8c61eaa0f119fff9d7d71227b3109867
DarkusStorm <tico.alvessilva2008@gmail.com>
2026-02-02 16:38:40 -0300

    adição do sqlite3

--- models/tarefa.py
+++ models/tarefa.py
@@ -1,13 +1,28 @@
+from models.database import Database
+from typing import Self
+
 class Tarefa:
+        def __init__(self: Self, titulo_tarefa: str, data_conclusao: str =None) -> None:
+                self.titulo_tarefa: str = titulo_tarefa
+                self.data_conclusao: str = data_conclusao
+                # Atrbutos que pertencem ao OBJETO.
 
-    lista_tarefas = []
-    def __init__(self, titulo_tarefa, data_conclusao=None):
-        self.titulo_tarefa = titulo_tarefa
-        self.data_conclusao = data_conclusao
-        # Atrbutos que pertencem ao OBJETO.
-        Tarefa.lista_tarefas.append(self)
+        def salvar_tarefa(self: Self) -> None:
+                with Database('./data/tarefas.sqlite3') as db:
+                        query: str = "INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
+                        params: tuple = (self.titulo_tarefa, self.data_conclusao)
+                        db.executar(query, params)
 
-    def listar_tarefas(cls, titulo_tarefa):
-        for tarefa in cls.lista_tarefas:
-            if tarefa.titulo_tarefa == titulo_tarefa:
-                print(tarefa.titulo_tarefa + ' - ' + tarefa.data_conclusao)
+# class Tarefa:
+
+#     lista_tarefas = []
+#     def __init__(self, titulo_tarefa: str, data_conclusao: str =None) -> None:
+#         self.titulo_tarefa: str = titulo_tarefa
+#         self.data_conclusao: str = data_conclusao
+#         # Atrbutos que pertencem ao OBJETO.
+#         self.lista_tarefas.append(self)
+
+#     def listar_tarefas(cls, titulo_tarefa):
+#         for tarefa in cls.lista_tarefas:
+#             if tarefa.titulo_tarefa == titulo_tarefa:
+#                 print(tarefa.titulo_tarefa + ' - ' + tarefa.data_conclusao)
